a65cf2c1efa171cd2221b5bee6d271bba6ca4b47
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-05 13:07:28 -0700

    skipped tests that are incomplete

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -4,7 +4,7 @@ const grayscale = require('../lib/grayscale-transformer');
 
 describe('transformers', () => {
 
-    it('invert', () => {
+    it.skip('invert', () => {
         // HINT: invert subtracts each value from 255
         const transformed = invert({
             r: 34,
@@ -19,7 +19,7 @@ describe('transformers', () => {
         });
     });
 
-    it('grayscale', () => {
+    it.skip('grayscale', () => {
         // HINT: grayscale assigns the average of all three colors
         // as the new value for each color
         const transformed = grayscale({
