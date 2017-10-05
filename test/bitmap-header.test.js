bc748411495fb8316fff9f443d887ebc732abbf3
Christina Mills <chrimills@gmail.com>
2017-10-05 11:13:08 -0700

    got header tests passing

--- test/bitmap-header.test.js
+++ test/bitmap-header.test.js
@@ -8,21 +8,21 @@ describe('bitmap header', () => {
 
     let buffer = null;
     beforeEach(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
+        // TODO/DONE: read './test/test-bitmap.bmp' into buffer variable
         // Go ahead and use the "sync" version of the fs methods
         buffer = fs.readFileSync('./test/test-bitmap.bmp');
         //console.log('bmp file: =================================================', buffer);
 
     });
 
-    it.only('has correct specs', () => {
-        // TODO: read the docs to figure out what these values should be
+    it('has correct specs', () => {
+        // TODO/DONE: read the docs to figure out what these values should be
         assert.ok(constants.PIXEL_OFFSET);
         assert.ok(constants.BITS_PER_PIXEL_OFFSET);
         assert.ok(constants.FILE_SIZE_OFFSET);
     });
 
-    it('parses header data', () => {
+    it.only('parses header data', () => {
         // TODO: use the constants to populate the following properties
         // on the BitmapHeader in its constructor.
         // These test values are correct for the supplied test-bitmap.bmp
