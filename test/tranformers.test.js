d424b23cee04674beff17e354a15f198327bfa1b
mugworks <greenwood.pdx@gmail.com>
2017-10-07 12:11:11 -0700

    luminosity test added

--- test/tranformers.test.js
+++ test/tranformers.test.js
@@ -1,6 +1,8 @@
 const assert = require('assert');
 const invert = require('../lib/invert-transformer');
 const grayscale = require('../lib/grayscale-transformer');
+const luminosity = require('../lib/luminosity-transformer');
+
 
 describe('transformers', () => {
 
@@ -19,7 +21,7 @@ describe('transformers', () => {
         });
     });
 
-    it.only('grayscale', () => {
+    it.skip('grayscale', () => {
         // HINT: grayscale assigns the average of all three colors
         // as the new value for each color
         const transformed = grayscale({
@@ -36,4 +38,18 @@ describe('transformers', () => {
     });
 
     // TODO: add a third transformer
+    it.skip ('luminosity', () => {
+        //uses weighted values of average
+        const transformed = luminosity({
+            r: 42,
+            g: 109,
+            b: 149
+        });
+
+        assert.deepEqual(transformed, {
+            r: 21,
+            g: 72,
+            b: 7
+        });
+    });
 });
